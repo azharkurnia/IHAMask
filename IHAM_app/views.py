3687f437efe4293e9057331490c8664601990772
Azhar Kurnia <azharkurnia19@gmail.com>
2018-01-08 11:13:23 +0700

    baru select blm select2

--- IHAM_app/views.py
+++ IHAM_app/views.py
@@ -6,6 +6,7 @@ import json
 response = {}
 
 def form_test(request):
+    response["provinces"] = get_province(request)['rajaongkir']['results']
     return render(request, 'formTest.html', response)
 
 def add_data(request):
@@ -26,7 +27,8 @@ def get_province(request):
     data = res.read()
     data = data.decode("utf-8")
     data = json.loads(data)
+    return data
     # print(data.decode("utf-8"))
     # print(type(data))
     print(data['rajaongkir']['status']['description'])
-    print(data['rajaongkir']['results'][0])
+    
