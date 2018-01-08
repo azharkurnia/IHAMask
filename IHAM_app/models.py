12111a9e5722f7d7d6e392617da07eadec3777c2
faisal ridwan <faisalridwan55@gmail.com>
2018-01-08 22:09:26 +0700

    model buat order list, di update dari halaman form, ditampilin di admin

--- IHAM_app/models.py
+++ IHAM_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class OrderList(models.Model):
+    customerName = models.CharField(max_length=140, blank=False)
+    customerEmail = models.EmailField(max_length=140, blank=False)
+    customerPhone = models.IntegerField(max_length=14, blank=False)
+    productQuantityA = models.IntegerField(max_length=5)
+    productQuantityB = models.IntegerField(max_length=5)
+    customerAddress = models.CharField(max_length=140, blank=False)
+    totalPrice = models.IntegerField(max_length=10, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
