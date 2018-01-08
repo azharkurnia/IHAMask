8310a803769596871e03c172e13a86fae2de618f
faisal ridwan <faisalridwan55@gmail.com>
2018-01-08 22:11:09 +0700

    benerin models

--- IHAM_app/models.py
+++ IHAM_app/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class OrderList(models.Model):
     customerName = models.CharField(max_length=140, blank=False)
     customerEmail = models.EmailField(max_length=140, blank=False)
-    customerPhone = models.IntegerField(max_length=14, blank=False)
-    productQuantityA = models.IntegerField(max_length=5)
-    productQuantityB = models.IntegerField(max_length=5)
+    customerPhone = models.IntegerField(blank=False)
+    productQuantityA = models.IntegerField()
+    productQuantityB = models.IntegerField()
     customerAddress = models.CharField(max_length=140, blank=False)
-    totalPrice = models.IntegerField(max_length=10, blank=False)
+    totalPrice = models.IntegerField(blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
