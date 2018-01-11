8cd60fc71bccabb6eb5cbfb5d953f9d6954c003c
fajrinmk <fajrinmk@gmail.com>
2018-01-11 23:19:56 +0700

    udah sabi nambah promo code ke database

--- IHAM_loginAdmin/views.py
+++ IHAM_loginAdmin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from .models import PromoCode
+from django.http import HttpResponseRedirect
 # Create your views here.
 response = {}
 def show_login(request):
