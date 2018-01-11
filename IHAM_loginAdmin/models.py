e9fec2ec77cb5119bf4456249bcccff733e632c0
fajrinmk <fajrinmk@gmail.com>
2018-01-11 11:37:21 +0700

    siapkan models untuk promocode admin

--- IHAM_loginAdmin/models.py
+++ IHAM_loginAdmin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class PromoCode(models.Model):
+    promoName = models.CharField(max_length=10, blank=False)
+    promoAmount = models.IntegerField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
