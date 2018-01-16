8865018a4aaf78010e7caabc3c8a6b620c5fa066
Favian Kharisma Hazman <vianhazman@gmail.com>
2018-01-17 04:38:58 +0700

    check requirements.txt update dependcies, switched to mySQL server

--- IHAMask/settings.py
+++ IHAMask/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'IHAMask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ihamaskc_data',
+        'USER': 'ihamaskc_admin',
+        'PASSWORD': 'ihamask',
+        'HOST': 'ihamask.com',
+        'PORT': '3306',
     }
 }
 
