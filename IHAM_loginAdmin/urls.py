7d6ea15aaa1a338fdc4207606364a32cbdbd5adb
fajrinmk <fajrinmk@gmail.com>
2018-01-21 17:09:38 +0700

    implement logout

--- IHAM_loginAdmin/urls.py
+++ IHAM_loginAdmin/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from .views import *
 urlpatterns = [
     url(r'^$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'),
     url(r'^admin/', admin.site.urls),
     url(r'^adminIHA/', logged_in, name = 'logged_in'),
     url(r'^add_promo_code/', add_promo_code, name = 'add_promo_code'),
