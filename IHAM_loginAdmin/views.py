481d579b6bfde968e5db38612afe8ce0f32f24df
Favian Kharisma Hazman <vianhazman@gmail.com>
2018-01-23 03:28:16 +0700

    admin

--- IHAM_loginAdmin/views.py
+++ IHAM_loginAdmin/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from .models import PromoCode
-from IHAM_app.models import OrderList
+from IHAM_app.models import OrderList, FAQ, upcomingEvents
 from django.http import HttpResponseRedirect
 # Create your views here.
 response = {}
@@ -13,8 +13,12 @@ def show_login(request):
 def logged_in(request):
 	promoCodeList = PromoCode.objects.all()
 	orderList = OrderList.objects.all()
+	faq = FAQ.objects.all()
+	upcomingevents = upcomingEvents.objects.all()
 	response['promoCodeList'] = promoCodeList
 	response['orderList'] = orderList
+	response['FAQ'] = faq
+	response['upcomingEvents'] = upcomingevents
 	return render(request, 'logged_in.html', response)
 
 @login_required
@@ -26,15 +30,50 @@ def add_promo_code(request):
 		print("amount " + str(request.POST['amount']))
 		promoCodeList = PromoCode(promoCode=response['code'],promoAmount=response['amount'])
 		promoCodeList.save()
+		return HttpResponseRedirect('/login/adminIHA/#form promo')
+	else:
 		return HttpResponseRedirect('/login/adminIHA/')
+
+@login_required
+def add_event(request):
+	if(request.method == 'POST'):
+		response['eventName'] = request.POST['eventName']
+		response['eventURL'] = request.POST['eventURL']
+		response['eventDate'] = request.POST['eventDate']
+		response['eventVenue'] = request.POST['eventVenue']
+		eventList = upcomingEvents(eventName=response['eventName'],eventURL=response['eventURL'],eventDate=response['eventDate'],eventVenue=response['eventVenue'])
+		eventList.save()
+		return HttpResponseRedirect('/login/adminIHA/#form event')
+	else:
+		return HttpResponseRedirect('/login/adminIHA/')
+
+@login_required
+def add_faq(request):
+	if(request.method == 'POST'):
+		response['titleIndo'] = request.POST['titleIndo']
+		response['titleEng'] = request.POST['titleEng']
+		response['isiIndo'] = request.POST['isiIndo']
+		response['isiEng'] = request.POST['isiEng']
+		faqlist = FAQ(titleIndo=response['titleIndo'],titleEng=response['titleEng'],isiIndo=response['isiIndo'],isiEng=response['isiEng'])
+		faqlist.save()
+		return HttpResponseRedirect('/login/adminIHA/#form faq')
 	else:
 		return HttpResponseRedirect('/login/adminIHA/')
 
 @login_required
 def delete_code(request, code_id):
-	print("delete")
 	PromoCode.objects.filter(id=code_id).delete()
-	return HttpResponseRedirect('/login/adminIHA/')
+	return HttpResponseRedirect('/login/adminIHA/#form promo')
+
+@login_required
+def delete_faq(request, code_id):
+	FAQ.objects.filter(id=code_id).delete()
+	return HttpResponseRedirect('/login/adminIHA/#form faq')
+
+@login_required
+def delete_event(request, code_id):
+	upcomingEvents.objects.filter(id=code_id).delete()
+	return HttpResponseRedirect('/login/adminIHA/#form event')
 
 @login_required
 def paidSlideTrue(request, paidTrue_id):
